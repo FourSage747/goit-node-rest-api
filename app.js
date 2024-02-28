72b40ab67da71b6a8a0a0195f0452283717cd6c8
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-02-28 22:46:34 +0100

    дз3

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
 const express = require("express")
-// // const morgan = require("morgan")
-// const cors = require("cors")
+// const morgan = require("morgan")
+const cors = require("cors")
 const mongoose = require("mongoose")
 
 const DB_HOST = 'mongodb+srv://FourSage:rutor26762676@cluster0.afo4ruu.mongodb.net/db-contacts?retryWrites=true&w=majority'
 
 const app = express();
 
-mongoose.set('strictQuery', true)
-mongoose.connect(DB_HOST)
-  .then(
-    app.listen(3000, () => {
-      console.log("Server is running. Use our API on port: 3000");
-    })
-  )
-  .catch((error)=>console.log(error.message))
 
-// const contactsRouter = require("./routes/contactsRouter.js")
+const contactsRouter = require("./routes/contactsRouter.js")
 
 
-// // app.use(morgan("tiny"));
-// app.use(cors());
-// app.use(express.json());
+// app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
 
-// app.use("/api/contacts", contactsRouter);
+app.use("/api/contacts", contactsRouter);
 
-// app.use((_, res) => {
-//   res.status(404).json({ message: "Route not found" });
-// });
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
 
-// app.use((err, req, res, next) => {
-//   const { status = 500, message = "Server error" } = err;
-//   res.status(status).json({ message });
-// });
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
 
 // app.listen(3000, () => {
 //   console.log("Server is running. Use our API on port: 3000");
 // });
+mongoose.set('strictQuery', true)
+mongoose.connect(DB_HOST)
+  .then(
+    app.listen(3000)
+  )
+  .catch((error)=>{
+    console.log(error.message)
+    process.exit(1)
+  })
