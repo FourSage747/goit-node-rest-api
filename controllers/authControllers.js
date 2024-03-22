90355b7cef77521f6bb381b1158fa6b8266273a6
FourSage747 <129331961+FourSage747@users.noreply.github.com>
2024-03-22 11:39:17 +0100

    дз5

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken")
 const User = require("../models/user")
 const gravatar = require("gravatar")
 const path = require("path")
-// const fs = require("fs/promises")
+const fs = require("fs/promises")
 const Jimp = require("jimp")
 
 
@@ -107,19 +107,22 @@ const updateAvatar = async(req, res, next) => {
     try {
         // const {_id} = req.user
         // const {path: tempUpload, originalname} = req.file
-        // const editImage = originalname.resize(250, 250, Jimp.RESIZE_BEZIER)
-        // const filename = `${_id}_${editImage}`
+        // const filename = `${_id}_${originalname}`
         // const resultUpload = path.join(avatarsDir, filename)
         // await fs.rename(tempUpload, resultUpload)
         // const avatarURL = path.join("avatars", filename)
         // await User.findByIdAndUpdate(_id, {avatarURL})
         // res.json({avatarURL})
         const {_id} = req.user;
+        if (!req.file) {
+            throw HttpError(400, "You have not uploaded a file")
+        }
         const {path: tempUpload, originalname} = req.file;
         const image = await Jimp.read(tempUpload);
         await image.resize(250, 250, Jimp.RESIZE_BEZIER);
         const filename = `${_id}_${originalname}`;
         const resultUpload = path.join(avatarsDir, filename);
+        await fs.rename(tempUpload, resultUpload)
         await image.writeAsync(resultUpload);
         const avatarURL = path.join("avatars", filename);
         await User.findByIdAndUpdate(_id, {avatarURL});
